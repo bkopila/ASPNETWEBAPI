cc22e93d6efeeb7bf0e97d7a87fcdc3cadb2883e
kopilabhattarai006-blip <kopilabhattarai006@gmail.com>
2026-03-25 08:01:26 +0545

    added connectionstr in appsetting

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,5 +1,4 @@
 using Microsoft.EntityFrameworkCore;
-using ProductAPI;
 
 public class AppDbContext : DbContext
 {
--- Models/Category.cs
+++ Models/Category.cs
@@ -1,8 +1,7 @@
-namespace ProductAPI;
 
 public class Category
 {
-    public int Id{get;set;}
-    public string Name {get;set;}
-    public ICollection<Product> Products {get;set;}
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public ICollection<Product> Products { get; set; }
 }
