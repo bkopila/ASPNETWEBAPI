2111508052a2425f3c61889799b98beb304d6fdb
kopilabhattarai006-blip <kopilabhattarai006@gmail.com>
2026-03-25 07:50:37 +0545

    added models

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,13 +1,22 @@
-namespace ProductAPI;
+using ProductAPI;
 
 public class Product
 {
     public int Id { get; set; }
+
     public string Name { get; set; }
+
+    public string SKU { get; set; }
     public decimal Price { get; set; }
+    public int Stock { get; set; }
+    public int SupplierId { get; set; }
+    public Supplier Supplier { get; set; }
 
     public string? Description { get; set; }
-    public string Category { get; set; }
+    public int CategoryId { get; set; }
+
+    public Category Category { get; set; }
     public string ImageUrl { get; set; }
+    public ICollection<OrderItem> OrderItems { get; set; }
 
 }
